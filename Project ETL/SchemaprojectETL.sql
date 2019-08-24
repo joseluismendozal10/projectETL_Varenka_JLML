2e9969bdb6919e5227594ecdbd01a69ef4247cf4
joseluismendozal10 <50883284+joseluismendozal10@users.noreply.github.com>
2019-08-23 21:38:37 -0500

    Add files via upload

---
+++ Project ETL/SchemaprojectETL.sql
@@ -0,0 +1,13 @@
+CREATE TABLE temptopcities (
+	index int,
+	Year INT,
+	City TEXT,
+	Country TEXT,
+	AverageTemperature FLOAT,
+	AverageTemperatureUncertainty FLOAT,
+	Population int
+);
+DROP TABLE temptopcities
+
+
+SELECT * FROM temptopcities
